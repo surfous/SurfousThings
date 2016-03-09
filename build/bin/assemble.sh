a1c31e28962f479bfcfb64b71f65221ef7ecddb7
Kevin Shuk <surf@surfous.com>
2016-03-08 17:48:40 -0800

    Convert scripts to new project manifest
    First crack at making build scripts work with the new dir structure

--- build/bin/assemble.sh
+++ build/bin/assemble.sh
@@ -9,7 +9,7 @@ export PROJECT_PATH="$($SCRIPT_PATH/path.sh)"
 
 SCRIPT_PATH="$($SCRIPT_PATH/path.sh script)"
 
-TMP_PATH="$($SCRIPT_PATH/path.sh tmp)"
+PROJECT_TMP_PATH="$($SCRIPT_PATH/path.sh tmp)"
 LIB_PATH="$($SCRIPT_PATH/path.sh lib)"
 source "$SCRIPT_PATH/commons.sh"
 
@@ -18,9 +18,9 @@ OPT_DEBUG=false # debug mode
 
 USAGE() {
 	echo "$SCRIPTNAME"
-	echo "${REV}Usage${BOLD} $SCRIPTNAME [-d] TARGET_SOURCE_PATH${NORM}"
+	echo "${REV}Usage${BOLD} $SCRIPTNAME [-d] ARTIFACT_SOURCE_PATH${NORM}"
 	echo "${REV}Parameters${NORM}"
-	echo " ${BOLD}TARGET_SOURCE_PATH${NORM}        the code file to be copied and assembled"
+	echo " ${BOLD}ARTIFACT_SOURCE_PATH${NORM}        the code file to be copied and assembled"
 	echo "${REV}Options${NORM}"
 	echo " ${BOLD}-d${NORM}                        Sets debug mode (don't clear away temp build dir & files)"
 	echo "\n"
@@ -63,31 +63,57 @@ shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
 ### End getopts code ###
 #
 # parse arguments
-TARGET_SOURCE_PATH="$1"
-if [ ! -e "$TARGET_SOURCE_PATH" ] || [ ! -f "$TARGET_SOURCE_PATH" ] || [ ! -r "$TARGET_SOURCE_PATH" ]; then
-	errorexit 10 "problem accessing '$TARGET_SOURCE_PATH'. Target source must exist, be a regular file, and be readable"
+ARTIFACT_SOURCE_PATH="$1"
+if [ ! -e "$ARTIFACT_SOURCE_PATH" ] || [ ! -f "$ARTIFACT_SOURCE_PATH" ] || [ ! -r "$ARTIFACT_SOURCE_PATH" ]; then
+	errorexit 10 "problem accessing '$ARTIFACT_SOURCE_PATH'. Target source must exist, be a regular file, and be readable"
 fi
-ARTIFACT_FILE="$(basename $TARGET_SOURCE_PATH)"
+ARTIFACT_FILE="$(basename $ARTIFACT_SOURCE_PATH)"
+ARTIFACT_TYPE="$(basename $(dirname $ARTIFACT_SOURCE_PATH))"
 ARTIFACT_EXTENSION="${ARTIFACT_FILE##*.}"
 ARTIFACT_NAME="${ARTIFACT_FILE%.*}"
+ARTIFACT_DIR="${ARTIFACT_NAME}.src"
+
+# Key to the build location variables
+# SurfousThings/build/tmp/                                                                                              $PROJECT_TMP_PATH
+#                         devicetypes-                                                                                     $ARTIFACT_TYPE
+#                         smartapps-                                                                                             "
+#                                     name_of_devicetype                                                                   $ARTIFACT_NAME
+#                                     name_of_smartapp                                                                           "
+#                                                       -#####/
+#                         ------------------------------------/                                                            $TMP_BUILD_DIR
+#-------------------------------------------------------------/                                                           $TMP_BUILD_PATH
+#                                                              name_of_devicetype.src/                                      $ARTIFACT_DIR
+#                                                              name_of_smartapp.src/                                             "
+#------------------------------------------------------------------------------------/                               $ARTIFACT_BUILD_PATH
+#                                                                                     name_of_devicetype.groovy            $ARTIFACT_FILE
+#                                                                                     name_of_smartapp.groovy              $ARTIFACT_FILE
+#-------------------------------------------------------------------------------------------------------------- $ARTIFACT_BUILD_FILE_PATH
+#
+#
 
 # setup temp dir for the build
-BUILDTMP=$($MKTEMPCMD -d "$TMP_PATH/assembly-${ARTIFACT_NAME}-XXXXX")
+TMP_BUILD_DIR="build-${ARTIFACT_TYPE}-${ARTIFACT_NAME}-XXXXX"
+TMP_BUILD_PATH=$($MKTEMPCMD -d "$PROJECT_TMP_PATH/$TMP_BUILD_DIR")
 if [ $? -ne 0 ]; then
 	errorexit 13 "could not create build temp directory" false
 fi
 
-# this is where we will assemble the final product
-TMP_BUILD_PATH="$BUILDTMP/$ARTIFACT_FILE"
+# this is where we will build the final product
+ARTIFACT_BUILD_PATH="$TMP_BUILD_PATH/$ARTIFACT_DIR"
+mkdir -p "$ARTIFACT_BUILD_PATH"
+if [ $? -ne 0 ]; then
+	errorexit 14 "could not create the artifact build directory" false
+fi
+ARTIFACT_BUILD_FILE_PATH="$ARTIFACT_BUILD_PATH/$ARTIFACT_FILE"
 
 # copy target source file to assemble directory
-cp $TARGET_SOURCE_PATH $TMP_BUILD_PATH
+cp $ARTIFACT_SOURCE_PATH $ARTIFACT_BUILD_PATH
 
 # tag build with datetime+pid: 20151031-121212.000
-$SCRIPT_PATH/buildtag.sh tag $TMP_BUILD_PATH
+$SCRIPT_PATH/buildtag.sh tag $ARTIFACT_BUILD_FILE_PATH
 
 # find the targets
-TARGET_LIST="$($SCRIPT_PATH/replace.sh -l $TMP_BUILD_PATH)"
+TARGET_LIST="$($SCRIPT_PATH/replace.sh -l $ARTIFACT_BUILD_FILE_PATH)"
 echo "$TARGET_LIST"
 
 # Loop over targets
@@ -107,7 +133,7 @@ while read -r target_line; do
 	addin_source_path=$addin_candidates
 
 	if [ ! -e "$addin_source_path" ]; then
-		errorexit 14 "Add-in file not found at path '$addin_source_path'"
+		errorexit 15 "Add-in file not found at path '$addin_source_path'"
 	fi
 
 	addin_source_file=$(basename "$addin_source_path")
@@ -123,20 +149,20 @@ while read -r target_line; do
 		replace_cmd+=" -r $addin_source_region"
 	fi
 	extract_cmd+=" $addin_source_path"
-	replace_cmd+=" $TMP_BUILD_PATH $addin_source_name"
+	replace_cmd+=" $ARTIFACT_BUILD_FILE_PATH $addin_source_name"
 
 	# execute the extract command to pull out the region
 	# echo "running extract: $extract_cmd"
 	extracted_region="$($extract_cmd)"
 	if [ $? -ne 0 ]; then
-		errorexit 15 "failed to extract $region_text from $addin_path"
+		errorexit 16 "failed to extract $region_text from $addin_path"
 	fi
 
 	# store the extracted region
 	extracted_region_name="$addin_source_name-$addin_source_region"
 	extracted_region_name="${extracted_region_name%-}"
 	extracted_region_file="$extracted_region_name.$addin_source_extension"
-	extracted_region_path=$($MKTEMPCMD "$BUILDTMP/region-${extracted_region_file}")
+	extracted_region_path=$($MKTEMPCMD "$TMP_BUILD_PATH/region-${extracted_region_file}")
 	echo "// REGION BEGIN - ORIGIN $addin_source_file $region_text" > "$extracted_region_path"
 	echo "$extracted_region" >> "$extracted_region_path"
 	echo "// REGION END - ORIGIN $addin_source_file $region_text" >> "$extracted_region_path"
@@ -146,45 +172,52 @@ while read -r target_line; do
 	# echo "running replace: $replace_cmd"
 	$($replace_cmd)
 	if [ $? -ne 0 ]; then
-		errorexit 16 "failed to replace target $addin_source_file $region_text in $TMP_BUILD_PATH"
+		errorexit 17 "failed to replace target $addin_source_file $region_text in $ARTIFACT_BUILD_FILE_PATH"
 	fi
 
 done <<< "$TARGET_LIST"
 
-# the artifact should be fully assembed in the build temp dir at TMP_BUILD_PATH at this point
+# the artifact should be fully assembled in the build temp dir at TMP_BUILD_PATH at this point
 
 # Move the assembled artifact to the assemble directory
-ASSEMBLE_DIR="$($SCRIPT_PATH/path.sh assemble)"
-OLD_ARTIFACT_DEST_DIR="$ASSEMBLE_DIR/archive"
-ARTIFACT_DEST_PATH="$ASSEMBLE_DIR/$ARTIFACT_FILE"
+ARTIFACT_ARCHIVE_DIR="$($SCRIPT_PATH/path.sh ${ARTIFACT_TYPE}_archive)"
+ARTIFACT_DEST_PATH="$($SCRIPT_PATH/path.sh ${ARTIFACT_TYPE})"
+ARTIFACT_DEST_FILE_PATH="${ARTIFACT_DEST_PATH}/${ARTIFACT_FILE}"
 
 ## Check the destination for a previously built artifact for the same source and move it
 if [ -e "$ARTIFACT_DEST_PATH" ]; then
 	# get the buidtag from the prior artifact
-	OLD_ARTIFACT_BUILDTAG="$($SCRIPT_PATH/buildtag.sh -g $ARTIFACT_DEST_PATH)"
+	OLD_ARTIFACT_BUILDTAG="$($SCRIPT_PATH/buildtag.sh -g $ARTIFACT_DEST_FILE_PATH)"
 	if [ $? -ne 0 ]; then
-		errorexit 17 "buildtag.sh get on $ARTIFACT_DEST_PATH exited with error"
+		errorexit 18 "buildtag.sh get on $ARTIFACT_DEST_FILE_PATH exited with error"
 	fi
 
-	# rename it using its buildtag
-	$($MKDIRCMD -p $OLD_ARTIFACT_DEST_DIR)
-	OLD_ARTIFACT_DEST_PATH="$OLD_ARTIFACT_DEST_DIR/$ARTIFACT_FILE.$OLD_ARTIFACT_BUILDTAG"
+	# move & rename the old version (directory and all) using its buildtag
+	$($MKDIRCMD -p $ARTIFACT_ARCHIVE_DIR)
+	OLD_ARTIFACT_DEST_PATH="$ARTIFACT_ARCHIVE_DIR/$ARTIFACT_NAME.$OLD_ARTIFACT_BUILDTAG"
 	mv "$ARTIFACT_DEST_PATH" "$OLD_ARTIFACT_DEST_PATH"
 	if [ $? -ne 0 ]; then
-		errorexit 18 "mv from $ARTIFACT_DEST_PATH to $OLD_ARTIFACT_DEST_PATH failed"
+		errorexit 19 "mv from $ARTIFACT_DEST_PATH to $OLD_ARTIFACT_DEST_PATH failed"
 	fi
 fi
 
-## Move the artifact into place
-mv "$TMP_BUILD_PATH" "$ARTIFACT_DEST_PATH"
+# compress the old artifact
+compress_cmd = "$(which zip) -r"
+$compress_cmd $OLD_ARTIFACT_DEST_PATH $OLD_ARTIFACT_DEST_PATH
 if [ $? -ne 0 ]; then
-	errorexit 18 "mv from $TMP_BUILD_PATH to $ARTIFACT_DEST_PATH failed"
+	errorexit 20 "compressing $OLD_ARTIFACT_DEST_PATH has failed"
+fi
+
+## Move the newly built artifact (directory and all) into place
+mv "$ARTIFACT_BUILD_PATH" "$ARTIFACT_DEST_PATH"
+if [ $? -ne 0 ]; then
+	errorexit 21 "mv from $ARTIFACT_BUILD_PATH to $ARTIFACT_DEST_PATH failed"
 fi
 
 # clean up tmp dir
 if [ "$OPT_DEBUG" = "false" ]; then
-	rm -rf $BUILDTMP
+	rm -rf $TMP_BUILD_PATH
 	if [ $? -ne 0 ]; then
-		errorexit 19 "'rm -rf $BUILDTMP' failed"
+		errorexit 22 "'rm -rf $TMP_BUILD_PATH' failed"
 	fi
 fi
--- build/bin/script/path.sh
+++ build/bin/script/path.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+declare -a TOP_LEVEL_DIRS=(build devicetypes smartapps)
+
 DEFAULT_CMD='default'
 PROJECT_CMD='project'
 TMP_CMD='tmp'
-ASSEMBLE_CMD='assemble'
-ARCHIVE_CMD='archive'
+APPS_CMD='smartapps'
+DEVICES_CMD='devicetypes'
+APP_ARCHIVE_CMD='smartapps_archive'
+DEVICE_ARCHIVE_CMD='devicetypes_archive'
 LIB_CMD='lib'
+BIN_CMD='bin'
 SCRIPT_CMD='script'
 
 CMD=$DEFAULT_CMD
@@ -13,33 +18,73 @@ if (( "$#" > "0" )); then
 	CMD=$1
 fi
 
-if [ $CMD == $PROJECT_CMD ] || [ $CMD == $DEFAULT_CMD ]; then
+if [ -z "$PROJECT_PATH" ]; then
 	SOURCE="${BASH_SOURCE[0]}"
-	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	while [ -h "$SOURCE" ]  # resolve $SOURCE until the file is no longer a symlink
+	do
 	  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 	  SOURCE="$(readlink "$SOURCE")"
 	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 	done
+
 	SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-	PROJECT_PATH=`dirname $SCRIPT_DIR`
-	echo $PROJECT_PATH
-	exit 0
+
+	PROJECT_PATH=""
+	CHECK_PATH="$(dirname $SCRIPT_DIR)"
+	echo "check dir path: $CHECK_PATH"
+
+	IS_TOP=false
+
+	while [[ "$IS_TOP" == false ]]
+	do
+		# Are the three subdirs that indicate that this is the project top directory here?
+		((DIR_MATCH_COUNT=0))
+		for tldir in "${TOP_LEVEL_DIRS[@]}"
+		do
+			CHECK_FOR_DIR="$CHECK_PATH/$tldir"
+			if [[ -d "$CHECK_FOR_DIR" ]]
+			then
+				((DIR_MATCH_COUNT+=1))
+			else
+				# Not this level - missing one
+				break
+			fi
+		done
+		if [ ${#TOP_LEVEL_DIRS[@]} -eq $DIR_MATCH_COUNT ]
+		then
+			IS_TOP=true
+			PROJECT_PATH=$CHECK_PATH
+			break
+		else
+			CHECK_PATH="$(dirname $CHECK_PATH)"
+		fi
+	done
+
+	if [[ $CHECK_PATH == "/" ]]
+	then
+		echo "Error: top level project directory not found"
+		exit 1
+	fi
 fi
 
-if [ -z "$PROJECT_PATH" ]; then
-	PROJECT_PATH=$($0)
-fi
-
-if [ $CMD == $TMP_CMD ]; then
-	echo "${PROJECT_PATH}/tmp"
-elif [ $CMD == $ASSEMBLE_CMD ]; then
-	echo "${PROJECT_PATH}/assemble"
-elif [ $CMD == $ARCHIVE_CMD ]; then
-	echo "${PROJECT_PATH}/assemble/archive"
+if [[ $CMD == $PROJECT_CMD || $CMD == $DEFAULT_CMD ]]; then
+	echo "${PROJECT_PATH}"
+elif [ $CMD == $TMP_CMD ]; then
+	echo "${PROJECT_PATH}/build/tmp"
+elif [ $CMD == $APPS_CMD ]; then
+	echo "${PROJECT_PATH}/build/smartapps"
+elif [ $CMD == $DEVICES_CMD ]; then
+	echo "${PROJECT_PATH}/build/decicetypes"
+elif [ $CMD == $APP_ARCHIVE_CMD ]; then
+	echo "${PROJECT_PATH}/build/smartapps/archive"
+elif [ $CMD == $DEVICE_ARCHIVE_CMD ]; then
+	echo "${PROJECT_PATH}/build/devicetypes/archive"
 elif [ $CMD == $LIB_CMD ]; then
-	echo "${PROJECT_PATH}/src/lib"
+	echo "${PROJECT_PATH}/build/src/lib"
+elif [ $CMD == $BIN_CMD ]; then
+	echo "${PROJECT_PATH}/build/bin"
 elif [ $CMD == $SCRIPT_CMD ]; then
-	echo "${PROJECT_PATH}/script"
+	echo "${PROJECT_PATH}/build/bin/script"
 else
 	echo "Error: Invalid command '$CMD'"
 	exit 1
